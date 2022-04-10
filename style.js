9cb3e1f72828f1773a7c91707082eb8c47074f41
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-04-10 22:39:26 +0200

    Basic files added

---
+++ style.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", load_style);
+
+//if ('serviceWorker' in navigator) { navigator.serviceWorker.register('sw.js').then(function(registration){registration.update()})}; //experimenteller Offlinemodus
+
+function load_style(){
+    //load_titlebar_stylesheet();
+}
