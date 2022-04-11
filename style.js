a62c18ac726ddffefe3ca94b541198703123d4f3
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2022-04-11 15:46:39 +0200

    First Titlebartest

--- style.js
+++ style.js
@@ -1,7 +1,12 @@
+var body;
+
 window.addEventListener("load", load_style);
 
 //if ('serviceWorker' in navigator) { navigator.serviceWorker.register('sw.js').then(function(registration){registration.update()})}; //experimenteller Offlinemodus
 
 function load_style(){
-    //load_titlebar_stylesheet();
+    body = document.getElementsByTagName("body")[0];
+    load_header_stylesheet();
+    load_titlebar_stylesheet();
+    load_content_stylesheet();
 }
---
+++ stylesheets/content/content.js
@@ -0,0 +1,9 @@
+function load_content_stylesheet(){
+    var content = document.getElementById("content");
+
+    load_content();
+}
+
+function load_content(){
+    
+}
